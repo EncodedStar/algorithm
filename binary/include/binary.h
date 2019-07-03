f702149ffb3505a57b49e9ebf9ccab081c35069c
EncodedStar <478672241@qq.com>
2019-07-03 11:34:42 +0800

    updata

---
+++ binary/include/binary.h
@@ -0,0 +1,9 @@
+#ifndef BINARY_H
+#define BINARY_H
+#include <iostream>
+
+namespace myLib
+{
+int count_binary(int n);
+}
+#endif
---
+++ binary/scr/binary.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include"binary.h"
+using namespace std;
+
+/************************************************** 
+ *  * @file binary.cpp
+ *  * @author EncodedStar
+ *  * @date 2019.7.13
+ *  * @function 计算n二进制的时候存在几个1
+ ***************************************************/
+
+namespace myLib
+{
+int count_binary(int n) {
+	int res = 0;
+	while (n != 0) {
+		n = n & (n - 1);
+		res++;
+	}
+	return res;
+}
+}
+
