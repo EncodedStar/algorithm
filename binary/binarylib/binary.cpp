44a7c871b8d379e183e656a27063cc516b24a12d
EncodedStar <478672241@qq.com>
2020-03-23 15:18:13 +0800

    updata

--- binary/binarylib/binary.cpp
+++ binary/binarylib/binary.cpp
@@ -57,5 +57,29 @@ namespace myLib
 		 return tmpv;
 	 }
 
+
+/************************************************** 
+ *  * @file binary.cpp
+ *  * @author EncodedStar
+ *  * @date 2020.3.23
+ *  * @function 开平方
+ ***************************************************/
+	float Q_rsqrt( float number )
+	{
+	    long i;
+	    float x2, y;
+	    const float threehalfs = 1.5F;
+	
+	    x2 = number * 0.5F;
+	    y  = number;
+	    i  = * ( long * ) &y; // evil floating point bit level hacking
+	    i  = 0x5f3759df - ( i >> 1 );  // what the fuck? 
+	    y  = * ( float * ) &i;
+	    y  = y * ( threehalfs - ( x2 * y * y ) );  // 1st iteration 
+	    // 2nd iteration, this can be removed
+	    //     // y  = y * ( threehalfs - ( x2 * y * y ) ); 
+	    return y;
+	}
+
 }
 
